0279b6473e6989b64eb3edc2fe6b61773f1be6da
justm1tch <57031880+just1mitch@users.noreply.github.com>
2024-05-22 21:56:30 +0800

    Modified SVM to use multiple iterations

--- SVM/create_classifications.m
+++ SVM/create_classifications.m
@@ -1,12 +1,13 @@
-% Get correct identifications
-path = "path_to_name_mapping.csv";
-
-metadata = readmatrix(path, OutputType='string');
-metadata = [metadata(:, 6), metadata(:, 1)];
-
-for i = 1:length(metadata)-1
-    volume = metadata(i+1, 1).split('_');
-    metadata(i+1, 1) = "volume_" +  volume(end);
+% Get correct classifications for glioma scans
+% Saves the classifications to a csv called 'classifications.csv
+function create_classifications(path)    
+    metadata = readmatrix(path, OutputType='string');
+    metadata = [metadata(:, 6), metadata(:, 1)];
+    
+    for i = 1:length(metadata)-1
+        volume = metadata(i+1, 1).split('_');
+        metadata(i+1, 1) = "volume_" +  volume(end);
+    end
+    
+    writematrix(metadata, ".\classifications.csv", 'WriteMode', 'overwrite', Delimiter=',');
 end
-
-writematrix(metadata, ".\classifications.csv", 'WriteMode', 'overwrite', Delimiter=',');
