ba84f7a79fbaa92b4bbd0c7471ba70fda124db99
justm1tch <57031880+just1mitch@users.noreply.github.com>
2024-05-23 23:23:08 +0800

    Final Cleanup
    Finished SVM using all combined features. Removed redundant training data from repository

---
+++ SVM/svm_helper_functions.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = svm_helper_functions(inputArg1,inputArg2)
+%SVM_HELPER_FUNCTIONS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
